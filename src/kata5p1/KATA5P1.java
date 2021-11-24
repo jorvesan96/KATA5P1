0c389bf7ce2cb246eaeac8c11843b2ead3e4a14f
jorve <jorve@DESKTOP-5E2NNK5>
2021-11-24 10:13:58 +0000

    Kata5 P1 version 2

--- src/kata5p1/KATA5P1.java
+++ src/kata5p1/KATA5P1.java
@@ -7,6 +7,8 @@ public class KATA5P1 {
     public static void main(String[] args) {
         SelectApp app = new SelectApp();
         app.instruction();
+        CrearTabla tabla = new CrearTabla();
+        tabla.createNewTable();
     }
 
 }
